832cf9665b11327f7b5285f6c6d300e64a5fc28b
Karolis <karolis.paulavicius2002@gmail.com>
2024-03-04 22:14:37 +0200

    Add search and filtering (#3)

---
+++ app/components/Navigation/Navigation.tsx
@@ -0,0 +1,115 @@
+"use client";
+import Link from "next/link";
+import Logo from "@/public/assets/logo_icon.svg";
+import LogoIcon from "@/public/assets/work.svg";
+import Image from "next/image";
+import { cookies } from "next/headers";
+import { useState } from "react";
+
+export default function Navigation() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <nav className="bg-gray-800">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-between">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            {/* <!-- Mobile menu button--> */}
+            <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+              <span className="absolute -inset-0.5"></span>
+              <span className="sr-only">Open main menu</span>
+              {/* <!--
+                Icon when menu is closed.
+
+                Menu open: "hidden", Menu closed: "block"
+              --> */}
+              <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+              </svg>
+              {/* <!--
+                Icon when menu is open.
+
+                Menu open: "block", Menu closed: "hidden"
+              --> */}
+              <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
+          </div>
+          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex flex-shrink-0 items-center">
+              <Image src={Logo} alt="Logo" className="h-8 w-auto" />
+              <span className="ml-2 text-white text-2xl font-bold ">IT Rėmėjas</span>
+            </div>
+            {/* <div className="hidden sm:ml-6 sm:block">
+              <div className="flex space-x-4">
+                <a href="#" className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Dashboard</a>
+                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Team</a>
+                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Projects</a>
+                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Calendar</a>
+              </div>
+            </div> */}
+          </div>
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            {/* <!-- Profile dropdown --> */}
+            <div className="relative ml-3">
+              <div>
+                <button 
+                  type="button" 
+                  className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" 
+                  id="user-menu-button" 
+                  aria-expanded={isMenuOpen}
+                  aria-haspopup="true"
+                  onClick={toggleMenu}
+                >
+                  <span className="absolute -inset-1.5"></span>
+                  <span className="sr-only">Open user menu</span>
+                  <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""></img>
+                </button>
+              </div>
+
+              {/* <!--
+                Dropdown menu, show/hide based on menu state.
+
+                Entering: "transition ease-out duration-100"
+                  From: "transform opacity-0 scale-95"
+                  To: "transform opacity-100 scale-100"
+                Leaving: "transition ease-in duration-75"
+                  From: "transform opacity-100 scale-100"
+                  To: "transform opacity-0 scale-95"
+              --> */}
+              <div 
+                className={`absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ${
+                  isMenuOpen ? "transition ease-out duration-100 transform opacity-100 scale-100" : "transition ease-in duration-75 transform opacity-0 scale-95"
+                }`} // Add this className attribute
+                role="menu" 
+                aria-orientation="vertical" 
+                aria-labelledby="user-menu-button" 
+                tabIndex={-1}
+                >
+                {/* <!-- Active: "bg-gray-100", Not Active: "" --> */}
+                <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-0">Mano profilis</a>
+                <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-1">Nustatymai</a>
+                <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-2">Atsijungti</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* <!-- Mobile menu, show/hide based on menu state. --> */}
+      {/* <div className="sm:hidden" id="mobile-menu">
+        <div className="space-y-1 px-2 pb-3 pt-2">
+          <a href="#" className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Dashboard</a>
+          <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Team</a>
+          <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projects</a>
+          <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Calendar</a>
+        </div>
+      </div> */}
+    </nav>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,167 @@
+"use client";
+import { useState, useEffect } from 'react';
+
+import ProjectCard from "./components/ProjectCard/ProjectCard";
+
+interface Project {
+  name: string;
+  short_description: string;
+  long_description: string;
+  repository: string;
+  logo: string; // Changed type to string
+  technology: string;
+  creation_date: string;
+  last_updated: string;
+  id_project: number;
+}
+
+export default function Home() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [sortBy, setSortBy] = useState('newest_updated');
+  const [searchQuery, setSearchQuery] = useState('');
+
+  useEffect(() => {
+    fetch("/api/project/all")
+      .then(res => res.json())
+      .then(data => {
+        const modifiedData = data.map((project: Project) => {
+          // Convert buffer to base64 string
+          const logoData = project.logo.data;
+          const base64String = Buffer.from(logoData).toString('base64');
+          return {
+            ...project,
+            logo: `data:image/jpeg;base64,${base64String}` // Assuming the logo is JPEG
+          };
+        });
+        setProjects(modifiedData);
+      })
+      .catch(err => console.error(err));
+  }, []);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  }
+
+  const handleSort = (sortBy: string) => {
+    setSortBy(sortBy);
+    setIsOpen(false);
+  }
+
+  const sortedProjects = () => {
+    if (sortBy === 'newest_updated') {
+      return projects.slice().sort((a, b) => new Date(b.last_updated).getTime() - new Date(a.last_updated).getTime());
+    } else if (sortBy === 'newest_created') {
+      return projects.slice().sort((a, b) => new Date(b.creation_date).getTime() - new Date(a.creation_date).getTime());
+    } else if (sortBy === 'oldest_updated') {
+      return projects.slice().sort((a, b) => new Date(a.last_updated).getTime() - new Date(b.last_updated).getTime());
+    } else if (sortBy === 'oldest_created') {
+      return projects.slice().sort((a, b) => new Date(a.creation_date).getTime() - new Date(b.creation_date).getTime());
+    } else {
+      return projects;
+    }
+  }
+
+  // Filter projects by search query
+  const filteredProjects = sortedProjects().filter(project => {
+    const isNameDescriptionMatch = project.name.toLowerCase().includes(searchQuery.toLocaleLowerCase()) ||
+    project.short_description.toLowerCase().includes(searchQuery.toLocaleLowerCase());
+
+    const isTagMatch = project.technology.toLowerCase().split(' ').some(tag => 
+      tag.includes(searchQuery.toLocaleLowerCase())
+    );
+
+    return isNameDescriptionMatch || isTagMatch;
+  });
+
+  return (    
+    <div className="flex flex-col items-center" >
+      {/* Search bar */}
+      <div className='flex items-center'>
+      <div className="pt-2 relative mx-auto text-gray-600">
+          <input 
+            className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+            type="search" 
+            name="search" 
+            placeholder="Ieškoti..."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          >
+          </input>
+          <button 
+            type="submit" 
+            className="absolute right-0 top-0 mt-5 mr-4">
+            <svg 
+              className="text-gray-600 h-4 w-4 fill-current" 
+              xmlns="http://www.w3.org/2000/svg"
+              version="1.1" 
+              x="0px" 
+              y="0px"
+              viewBox="0 0 56.966 56.966"
+              width="512px" 
+              height="512px">
+              <path
+                d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
+            </svg>
+          </button>
+        </div>
+
+        {/* Sort dropdown */}
+        <div className="relative inline-block text-left mt-2 ml">
+          <div>
+            <button
+              type="button"
+              className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 ml-2" 
+              id="menu-button"
+              aria-expanded="true"
+              aria-haspopup="true"
+              onClick={toggleDropdown}
+            >
+                Rikiuoti Pagal
+                <svg 
+                  className="-mr-1 h-5 w-5 text-gray-400" 
+                  viewBox="0 0 20 20" 
+                  fill="currentColor" 
+                  aria-hidden="true"
+                >
+                  <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
+                </svg>
+            </button>
+          </div>
+
+        {/* Sort dropdown content */}
+        {isOpen && (
+          <div 
+            className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" 
+            role="menu" 
+            aria-orientation="vertical" 
+            aria-labelledby="menu-button" 
+          >
+            <div className="py-1" role="none">
+              <button onClick={() => handleSort('newest_updated')} className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" id="menu-item-0">Naujausiai atnaujintą</button>
+              <button onClick={() => handleSort('newest_created')} className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" id="menu-item-1">Naujausiai sukurtą</button>
+              <button onClick={() => handleSort('oldest_updated')} className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" id="menu-item-2">Seniausiai atnaujintą</button>
+              <button onClick={() => handleSort('oldest_created')} className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" id="menu-item-3">Seniausiai sukurtą</button>
+            </div>
+          </div>
+        )}
+        </div>
+      </div>
+
+
+      {filteredProjects.map((project, index) => (
+        <div className="p-6 max-w-2xl max-h-60">
+        <ProjectCard 
+          image_url={project.logo}
+          title={project.name}
+          description={project.short_description}
+          timeUpdated={project.last_updated}
+          issueCount={0}
+          volunteerCount={0}
+          tags={project.technology.split(' ')}
+        />
+        </div>
+      ))}
+    </div>
+  );
+}
