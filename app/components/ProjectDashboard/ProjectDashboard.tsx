b71142402ac0f8a7ecd02f19362d9f50f544d539
rokenga <robertaruka@gmail.com>
2024-03-14 19:16:46 +0200

    Fixed width of the project card

--- app/components/ProjectDashboard/ProjectDashboard.tsx
+++ app/components/ProjectDashboard/ProjectDashboard.tsx
@@ -24,32 +24,32 @@ const ProjectDashboard = ({
   updated_at,
 }: ProjectDashboardProps) => {
   return (
-    <div className="flex flex-col items-center justify-center p-6 max-w-4xl mx-auto">
-      {/* project card details */}
-      <ProjectCard
-        image_url={logo}
-        title={name}
-        description={short_description}
-        timeUpdated={updated_at}
-        issueCount={0} 
-        volunteerCount={0} 
-        tags={tags}
-      />
-
-      {/* buttons after the project card */}
-      <div className="mt-4 flex justify-center gap-2">
-        <a href={repository} target="_blank" rel="noopener noreferrer" style={{ backgroundColor: '#40C173', color: '#000' }} className="py-2 px-4 rounded-lg hover:bg-green-700 transition duration-150 ease-in-out">Repositorija</a>
-        <a href="#" style={{ backgroundColor: '#40C173', color: '#000' }} className="py-2 px-4 rounded-lg hover:bg-green-700 transition duration-150 ease-in-out">Diskusijos</a>
-        <a href="#" style={{ backgroundColor: '#40C173', color: '#000' }} className="py-2 px-4 rounded-lg hover:bg-green-700 transition duration-150 ease-in-out">Pradėti naują diskusiją</a>
-        <a href="#" style={{ backgroundColor: '#C14040', color: '#000' }} className="py-2 px-4 rounded-lg hover:bg-red-700 transition duration-150 ease-in-out">Pranešti apie kritinę klaidą</a>
+    <div className="flex flex-col items-center justify-center p-6">
+      {/* Project card details */}
+      <div className="p-6 w-[800px] max-h-60"> 
+        <ProjectCard
+          image_url={logo}
+          title={name}
+          description={short_description}
+          timeUpdated={updated_at}
+          issueCount={0}
+          volunteerCount={0}
+          tags={tags}
+        />
       </div>
 
-      {/* description with markdown features */}
-      <div className="mt-4 long-description-right">
+      {/* Buttons after the project card */}
+      <div className="mt-4 flex justify-center gap-2">
+        <a href={repository} target="_blank" rel="noopener noreferrer" className="py-2 px-4 rounded-lg text-black bg-[#40C173] hover:bg-green-700 transition duration-150 ease-in-out">Repositorija</a>
+        <a href="#" className="py-2 px-4 rounded-lg text-black bg-[#40C173] hover:bg-green-700 transition duration-150 ease-in-out">Diskusijos</a>
+        <a href="#" className="py-2 px-4 rounded-lg text-black bg-[#40C173] hover:bg-green-700 transition duration-150 ease-in-out">Pradėti naują diskusiją</a>
+        <a href="#" className="py-2 px-4 rounded-lg text-black bg-[#C14040] hover:bg-red-700 transition duration-150 ease-in-out">Pranešti apie kritinę klaidą</a>
+      </div>
+
+      {/* Description with markdown features */}
+      <div className="mt-4 w-full max-w-lg">
         <MarkdownDisplay markdownText={long_description} />
       </div>
-
-
     </div>
   );
 };
