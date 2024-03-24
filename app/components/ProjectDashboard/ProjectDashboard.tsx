e4b654e921a0dea84d38d8b93de9f7ae23526a38
Karolis <68164180+Karolinskis@users.noreply.github.com>
2024-03-24 19:36:35 +0200

    Fix spacing

--- app/components/ProjectDashboard/ProjectDashboard.tsx
+++ app/components/ProjectDashboard/ProjectDashboard.tsx
@@ -1,59 +1,59 @@
 "use client";
-import ProjectCard from "../ProjectCard/ProjectCard"; 
-import MarkdownDisplay from "../MarkdownDisplay/MarkdownDisplay"; 
+import ProjectCard from "../ProjectCard/ProjectCard";
+import MarkdownDisplay from "../MarkdownDisplay/MarkdownDisplay";
 import Link from "next/link";
 
 interface ProjectDashboardProps {
-  name: string;
-  short_description: string;
-  long_description: string;
-  repository: string;
-  logo: string;
-  tags: string[];
-  created_at: string;
-  updated_at: string;
-  id_project: number;
+    name: string;
+    short_description: string;
+    long_description: string;
+    repository: string;
+    logo: string;
+    tags: string[];
+    created_at: string;
+    updated_at: string;
+    id_project: number;
 }
 
 const ProjectDashboard = ({
-  name,
-  short_description,
-  long_description,
-  repository,
-  logo,
-  tags,
-  updated_at,
-  id_project
+    name,
+    short_description,
+    long_description,
+    repository,
+    logo,
+    tags,
+    updated_at,
+    id_project
 }: ProjectDashboardProps) => {
-  return (
-    <div className="flex flex-col items-center justify-center p-6">
-      {/* Project card details */}
-      <div className="p-6 w-[800px] max-h-60"> 
-        <ProjectCard
-          image_url={logo}
-          title={name}
-          description={short_description}
-          timeUpdated={updated_at}
-          issueCount={0}
-          volunteerCount={0}
-          tags={tags}
-        />
-      </div>
+    return (
+        <div className="flex flex-col items-center justify-center p-6">
+            {/* Project card details */}
+            <div className="p-6 w-[800px] max-h-60">
+                <ProjectCard
+                    image_url={logo}
+                    title={name}
+                    description={short_description}
+                    timeUpdated={updated_at}
+                    issueCount={0}
+                    volunteerCount={0}
+                    tags={tags}
+                />
+            </div>
 
-      {/* Buttons after the project card */}
-      <div className="mt-4 flex justify-center gap-2">
-        <Link href={repository} target="_blank" rel="noopener noreferrer" className="py-2 px-4 rounded-lg text-black bg-[#40C173] hover:bg-green-700 transition duration-150 ease-in-out">Repositorija</Link>
-        <Link href="#" className="py-2 px-4 rounded-lg text-black bg-[#40C173] hover:bg-green-700 transition duration-150 ease-in-out">Klaidos</Link>
-        <Link href={`/project/${id_project}/fault/new`} className="py-2 px-4 rounded-lg text-black bg-[#40C173] hover:bg-green-700 transition duration-150 ease-in-out">Pranešti apie kritinę klaidą</Link>
-        <Link href={`/project/${id_project}/fault`} className="py-2 px-4 rounded-lg text-black bg-[#C14040] hover:bg-red-700 transition duration-150 ease-in-out">Kritinės klaidos</Link>
-      </div>
+            {/* Buttons after the project card */}
+            <div className="mt-4 flex justify-center gap-2">
+                <Link href={repository} target="_blank" rel="noopener noreferrer" className="py-2 px-4 rounded-lg text-black bg-[#40C173] hover:bg-green-700 transition duration-150 ease-in-out">Repositorija</Link>
+                <Link href="#" className="py-2 px-4 rounded-lg text-black bg-[#40C173] hover:bg-green-700 transition duration-150 ease-in-out">Klaidos</Link>
+                <Link href={`/project/${id_project}/fault/new`} className="py-2 px-4 rounded-lg text-black bg-[#40C173] hover:bg-green-700 transition duration-150 ease-in-out">Pranešti apie kritinę klaidą</Link>
+                <Link href={`/project/${id_project}/fault`} className="py-2 px-4 rounded-lg text-black bg-[#C14040] hover:bg-red-700 transition duration-150 ease-in-out">Kritinės klaidos</Link>
+            </div>
 
-      {/* Description with markdown features */}
-      <div className="mt-4 w-full max-w-lg">
-        <MarkdownDisplay markdownText={long_description} />
-      </div>
-    </div>
-  );
+            {/* Description with markdown features */}
+            <div className="mt-4 w-full max-w-lg">
+                <MarkdownDisplay markdownText={long_description} />
+            </div>
+        </div>
+    );
 };
 
 export default ProjectDashboard;
--- app/project/[id]/page.tsx
+++ app/project/[id]/page.tsx
@@ -3,81 +3,81 @@ import { useState, useEffect } from 'react';
 import ProjectDashboard from "../../components/ProjectDashboard/ProjectDashboard";
 
 interface Project {
-  id: number;
-  name: string;
-  short_description: string;
-  long_description: string;
-  repository: string;
-  technologies: string;
-  created_at: string;
-  updated_at: string;
-  star_count: number;
-  contributor_count: number;
-  codebase_visibility: string;
-  fk_imagesid_images: number;
+    id: number;
+    name: string;
+    short_description: string;
+    long_description: string;
+    repository: string;
+    technologies: string;
+    created_at: string;
+    updated_at: string;
+    star_count: number;
+    contributor_count: number;
+    codebase_visibility: string;
+    fk_imagesid_images: number;
 
-  logo: string;
-  images: {
-    image: {
-      data: Buffer;
-      contentType: string;
+    logo: string;
+    images: {
+        image: {
+            data: Buffer;
+            contentType: string;
+        }
     }
-  }
 }
 
 export default function ProjectPage({ params }: {
-  params: { id: number }
+    params: { id: number }
 }) {
-  const [project, setProject] = useState<Project | null>(null);
+    const [project, setProject] = useState<Project | null>(null);
 
-  const projectId = params.id;
+    const projectId = params.id;
 
-  useEffect(() => {
-    if (projectId) {
-      fetch(`/api/project/${projectId}`)
-        .then(res => res.json())
-        .then(data => {
-          if (data && data.images.image.data) {
-            const logoData = data.images.image.data
-            const base64String = Buffer.from(logoData).toString('base64');
-            const modifiedProject = {
-              ...data,
-              logo: `data:image/jpeg;base64,${base64String}`
-            };
-            setProject(modifiedProject);
-          } else {
-            console.error("No image data found");
-
-
-            setProject(data);
-          }
-        })
-        .catch(console.error);
-    }
-  }, [projectId]);
-
-  return (
-    <div>
-      {project ? (
-        <>
-          <ProjectDashboard
-            name={project.name}
-            short_description={project.short_description}
-            long_description={project.long_description}
-            repository={project.repository}
-            logo={project.logo}
-            tags={project.technologies.split(' ')}
-            created_at={project.created_at}
-            updated_at={project.updated_at}
-            id_project={project.id}
-          />
-
-        </>
-      ) : (
-        <p>Loading project details...</p>
-      )}
-    </div>
+    useEffect(() => {
+        if (projectId) {
+            fetch(`/api/project/${projectId}`)
+                .then(res => res.json())
+                .then(data => {
+                    if (data && data.images.image.data) {
+                        const logoData = data.images.image.data
+                        const base64String = Buffer.from(logoData).toString('base64');
+                        const modifiedProject = {
+                            ...data,
+                            logo: `data:image/jpeg;base64,${base64String}`
+                        };
+                        setProject(modifiedProject);
+                    } else {
+                        console.error("No image data found");
 
 
-  );
+                        setProject(data);
+                    }
+                })
+                .catch(console.error);
+        }
+    }, [projectId]);
+
+    return (
+        <div>
+            {project ? (
+                <>
+                    <ProjectDashboard
+                        name={project.name}
+                        short_description={project.short_description}
+                        long_description={project.long_description}
+                        repository={project.repository}
+                        logo={project.logo}
+                        tags={project.technologies.split(' ')}
+                        created_at={project.created_at}
+                        updated_at={project.updated_at}
+                        id_project={project.id}
+                    />
+
+                </>
+            ) : (
+                <p>Loading project details...</p>
+            )}
+        </div>
+
+
+    );
 }
