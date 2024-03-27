ebd42cda13f08dc62a87daef1cc0dad228aa56a8
Aistis Draugelis <aistis.draugelis@ktu.edu>
2024-03-27 18:36:45 +0200

    Changed buttons slightly

--- app/components/ProjectDashboard/ProjectDashboard.tsx
+++ app/components/ProjectDashboard/ProjectDashboard.tsx
@@ -42,10 +42,10 @@ const ProjectDashboard = ({
 
             {/* Buttons after the project card */}
             <div className="mt-4 flex justify-center gap-2">
-                <Link href={repository} target="_blank" rel="noopener noreferrer" className="py-2 px-4 rounded-lg text-black bg-[#40C173] hover:bg-green-700 transition duration-150 ease-in-out">Repositorija</Link>
-                <Link href="#" className="py-2 px-4 rounded-lg text-black bg-[#40C173] hover:bg-green-700 transition duration-150 ease-in-out">Klaidos</Link>
-                <Link href={`/project/${id_project}/fault/new`} className="py-2 px-4 rounded-lg text-black bg-[#40C173] hover:bg-green-700 transition duration-150 ease-in-out">Pranešti apie kritinę klaidą</Link>
-                <Link href={`/project/${id_project}/fault`} className="py-2 px-4 rounded-lg text-black bg-[#C14040] hover:bg-red-700 transition duration-150 ease-in-out">Kritinės klaidos</Link>
+                <Link href={repository} target="_blank" rel="noopener noreferrer" className="py-2 px-4 rounded-lg text-black bg-[#40C173] hover:bg-green-600 transition duration-150 ease-in-out">Repositorija</Link>
+                <Link href={`/project/${id_project}/issue`} className="py-2 px-4 rounded-lg text-black bg-[#ffeb4f] hover:bg-yellow-400 transition duration-150 ease-in-out">Trūkumai</Link>
+                <Link href={`/project/${id_project}/fault/new`} className="py-2 px-4 rounded-lg text-black bg-[#40C173] hover:bg-green-600 transition duration-150 ease-in-out">Pranešti apie klaidą</Link>
+                <Link href={`/project/${id_project}/fault`} className="py-2 px-4 rounded-lg text-black bg-[#C14040] hover:bg-red-800 transition duration-150 ease-in-out">Klaidos</Link>
             </div>
 
             {/* Description with markdown features */}
