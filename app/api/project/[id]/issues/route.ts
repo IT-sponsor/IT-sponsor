e4e8d5432a7ba7f2450cc778b4c784354af81997
Aistis Draugelis <aistis.draugelis@ktu.edu>
2024-03-27 18:37:30 +0200

    Backend for adding and viewing issues

---
+++ app/api/project/[id]/issues/route.ts
@@ -0,0 +1,13 @@
+import prisma from "@/app/utils/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+    request: NextRequest,
+    { params }: { params: { id: Number } }
+) {
+    console.log(params.id);
+    let issues = await prisma.issues.findMany({
+        where: { fk_projectsid: Number(params.id) }
+    });
+    return NextResponse.json(issues);
+}
