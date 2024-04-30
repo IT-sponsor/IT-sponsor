25af6826084213d84a44fd029195aa8ac1cc3ed7
MantasBac <49941995+MantasBac@users.noreply.github.com>
2024-04-30 10:23:26 +0300

    Assign project to user
    Assigns created project to the logged in user

--- app/project/new/page.tsx
+++ app/project/new/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { NextResponse } from 'next/server';
 
 import MarkdownEditor from '@/app/components/MarkdownEditor/MarkdownEditor';
+import { useSession } from 'next-auth/react';
 
 const NewProjectPage = () => {
   const [projectName, setProjectName] = useState('');
@@ -13,6 +14,7 @@ const NewProjectPage = () => {
   const [fullDescription, setFullDescription] = useState("# Apie įmonę:\n...\n# Apie projektą:\n...");
   const [image, setImage] = useState<File | null>(null);
   const [formErrors, setFormErrors] = useState<Record<string, any>>({});
+  const { data: session } = useSession();
 
   const validateForm = () => {
     const newErrors: Record<string, any> = {};
@@ -53,6 +55,13 @@ const NewProjectPage = () => {
 
       if (response.ok) {
         const result = await response.json();
+
+        await fetch(`/api/controls/${result.project.id}`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ projectId: result.project.id, userId: Number(session?.user.id)})
+        });
+
         window.location.href = `/project/${result.project.id}`;
       } else {
         return new NextResponse(JSON.stringify({ message: "Error creating project", error: response.statusText }), { status: 500, headers: { 'Content-Type': 'application/json' } });
