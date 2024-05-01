e865ab7b80a9d9947f0a041b2f004accb4ff9f34
Karolis <68164180+Karolinskis@users.noreply.github.com>
2024-05-01 17:26:58 +0300

    Add ability to edit image

--- app/api/project/[id]/route.ts
+++ app/api/project/[id]/route.ts
@@ -2,40 +2,107 @@ import prisma from "@/app/utils/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
-    request: NextRequest,
-    { params }: { params: { id: Number } }
+  request: NextRequest,
+  { params }: { params: { id: Number } }
 ) {
-    let project = await prisma.projects.findUnique({
-        where: { id: Number(params.id) },
-        include: {
-            images: true, // include related image data
-        },
-    });
-    return NextResponse.json(project);
+  let project = await prisma.projects.findUnique({
+    where: { id: Number(params.id) },
+    include: {
+      images: true, // include related image data
+    },
+  });
+  return NextResponse.json(project);
 }
 
 export async function PUT(
-    request: NextRequest,
-    { params }: { params: { id: Number } }
+  request: NextRequest,
+  { params }: { params: { id: Number } }
 ) {
-    const data = await request.json(); 
-    try {
-        const updatedProject = await prisma.projects.update({
-            where: { id: Number(params.id) },
-            data: {
-                name: data.name,
-                short_description: data.short_description,
-                long_description: data.long_description,
-                repository: data.repository,
-                technologies: data.technologies,
-                codebase_visibility: data.codebase_visibility,
-            },
-            include: {
-                images: true, 
-            },
-        });
-        return NextResponse.json(updatedProject);
-    } catch (error) {
-        return new NextResponse(JSON.stringify({ message: "Error updating project", error: error.message }), { status: 500 });
+  console.log(request.body);
+  const formData = await request.formData();
+  console.log(formData);
+
+  const name = formData.get("name") as string;
+  const short_description = formData.get("short_description") as string;
+  const long_description = formData.get("long_description") as string;
+  const repository = formData.get("repository") as string;
+  const technologies = formData.get("technologies") as string;
+  const star_count = Number(formData.get("star_count")) || 0;
+  const contributor_count = Number(formData.get("contributor_count")) || 0;
+  const codebase_visibility = formData.get("codebase_visibility") as string;
+  const image = (formData.get("image") as File) || null;
+
+  try {
+    let imageId = null;
+    if (image) {
+      const imageFormData = new FormData();
+      imageFormData.append("image", image);
+
+      const imageResponse = await fetch(
+        "http://localhost:3000/api/image/upload",
+        {
+          method: "POST",
+          body: imageFormData,
+        }
+      );
+
+      if (!imageResponse.ok) {
+        return new NextResponse(
+          JSON.stringify({ message: "Error uploading image" }),
+          { status: 500 }
+        );
+      }
+
+      const imageResponseData = await imageResponse.json();
+      imageId = imageResponseData.upload.id_images;
     }
+
+    const project = await prisma.projects.update({
+      where: { id: Number(params.id) },
+      data: {
+        name,
+        short_description,
+        long_description,
+        repository,
+        technologies,
+        star_count,
+        contributor_count,
+        codebase_visibility,
+        updated_at: new Date(),
+        fk_imagesid_images: imageId,
+      },
+    });
+
+    console.log("Project updated", project);
+    return new NextResponse(
+      JSON.stringify({ message: "Project updated successfully", project }),
+      { status: 200 }
+    );
+  } catch (e: any) {
+    return new NextResponse(
+      JSON.stringify({ message: "Error updating project", error: e.message }),
+      { status: 500 }
+    );
+  }
+
+  // const data = await request.json();
+  // try {
+  //     const updatedProject = await prisma.projects.update({
+  //         where: { id: Number(params.id) },
+  //         data: {
+  //             name: data.name,
+  //             short_description: data.short_description,
+  //             long_description: data.long_description,
+  //             repository: data.repository,
+  //             technologies: data.technologies,
+  //             codebase_visibility: data.codebase_visibility,
+  //         },
+  //         include: {
+  //             images: true,
+  //         },
+  //     });
+  //     return NextResponse.json(updatedProject);
+  // } catch (error) {
+  //     return new NextResponse(JSON.stringify({ message: "Error updating project", error: error.message }), { status: 500 });
+  // }
 }
--- app/project/[id]/edit/page.tsx
+++ app/project/[id]/edit/page.tsx
@@ -4,16 +4,33 @@ import Link from "next/link";
 import { NextResponse } from "next/server";
 import MarkdownEditor from "@/app/components/MarkdownEditor/MarkdownEditor";
 
+interface Project {
+    id: number;
+    name: string;
+    short_description: string;
+    long_description: string;
+    repository: string;
+    technologies: string;
+    created_at: string;
+    updated_at: string;
+    star_count: number;
+    contributor_count: number;
+    codebase_visibility: string;
+    fk_imagesid_images: number;
+
+    logo: string;
+    images: {
+        image: {
+            data: Buffer;
+            contentType: string;
+        }
+    }
+}
+
 export default function EditProjectPage({ params }: { params: { id: number } }) {
-    const [project, setProject] = useState({
-        name: '',
-        short_description: '',
-        long_description: '',
-        codebase_visibility: '',
-        repository: '',
-        technologies: '',
-    });
-    const [formErrors, setFormErrors] = useState({});
+    const [project, setProject] = useState<Project | null>(null);
+    const [image, setImage] = useState<File | null>(null);
+    const [formErrors, setFormErrors] = useState<Record<string, any>>({});
     const projectId = params.id;
 
     useEffect(() => {
@@ -22,31 +39,54 @@ export default function EditProjectPage({ params }: { params: { id: number } }) 
             if (response.ok) {
                 const projectData = await response.json();
                 setProject(projectData);
+                // This is the easiest way I could think of converting Buffer data to a File object
+                // NOTE: This is dumb, we need to stop saving images in the database
+                const blob = new Blob([new Uint8Array(projectData.images.image.data)], { type: projectData.images.image.contentType });
+                const file = new File([blob], 'ProjectImage.png', { type: projectData.images.image.contentType });
+
+                setImage(file);
             }
         };
+
         fetchProject();
     }, [projectId]);
 
     const validateForm = () => {
-        const errors = {};
-        if (!project.name.trim()) errors.name = "Projekto pavadinimas yra privalomas.";
-        if (!project.short_description.trim()) errors.shortDescription = "Trumpas aprašymas yra privalomas.";
-        if (!project.repository.trim()) errors.repository = "Repositorijos nuoroda yra būtina.";
-        if (!project.technologies.trim()) errors.technologies = "Technologijos yra privalomos.";
-        if (!project.long_description.trim()) errors.longDescription = "Pilnas aprašymas yra privalomas.";
+        const errors: Record<string, any> = {};
+        if (!project?.name.trim()) errors.name = "Projekto pavadinimas yra privalomas.";
+        if (!project?.short_description.trim()) errors.shortDescription = "Trumpas aprašymas yra privalomas.";
+        if (!project?.repository.trim()) errors.repository = "Repositorijos nuoroda yra būtina.";
+        if (!project?.technologies.trim()) errors.technologies = "Technologijos yra privalomos.";
+        if (!project?.long_description.trim()) errors.longDescription = "Pilnas aprašymas yra privalomas.";
         setFormErrors(errors);
         return Object.keys(errors).length === 0;
     };
 
-    const handleSubmit = async (e) => {
+    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files && e.target.files.length > 0) {
+            const file = e.target.files[0];
+            setImage(file);
+        }
+    }
+
+    const handleSubmit = async (e: any) => {
         e.preventDefault();
         if (!validateForm()) return;
+        if (!project) return;
+
+        const projectData = new FormData();
+        projectData.append('name', project.name);
+        projectData.append('short_description', project.short_description);
+        projectData.append('long_description', project.long_description);
+        projectData.append('repository', project.repository);
+        projectData.append('technologies', project.technologies);
+        projectData.append('codebase_visibility', project.codebase_visibility);
+        if (image) projectData.append('image', image);
 
         try {
             const response = await fetch(`/api/project/${projectId}`, {
                 method: 'PUT',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(project),
+                body: projectData
             });
             if (!response.ok) {
                 throw new Error(`Failed to update project: ${response.statusText}`);
@@ -67,6 +107,31 @@ export default function EditProjectPage({ params }: { params: { id: number } }) 
             <div className="px-6 py-5 rounded-xl border-2 border-gray-100 bg-white" style={{ width: '800px' }}>
                 <h1 className="text-2xl font-bold text-gray-800 text-center">Redaguoti projektą</h1>
                 <form onSubmit={handleSubmit} className="flex flex-col items-start justify-center w-full">
+                    <div id="image-preview" className="max-w-sm p-6 mb-4 bg-gray-100 border-dashed border-2 border-gray-400 rounded-lg items-center mx-auto text-center cursor-pointer mt-2">
+                        {image && (
+                            <label htmlFor="upload" className="cursor-pointer">
+                                <img
+                                    src={URL.createObjectURL(image)}
+                                    alt='project image'
+                                    className="max-h-48 rounded-lg mx-auto"
+                                />
+                            </label>
+                        ) || (
+                                <>
+                                    <label htmlFor="upload" className="cursor-pointer">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-8 h-8 text-gray-700 mx-auto mb-4">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+                                        </svg>
+                                        <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-700">Įkelti nuotrauką</h5>
+                                        <p className="font-normal text-sm text-gray-400 md:px-6">Pasirinkite nuotrauką, kurios dydis būtų mažesnis nei <b className="text-gray-600">2mb</b></p>
+                                        <p className="font-normal text-sm text-gray-400 md:px-6">leidžiami <b className="text-gray-600">JPG arba PNG</b> formatai.</p>
+                                        <span id="filename" className="text-gray-500 bg-gray-200 z-50"></span>
+                                    </label>
+                                </>
+                            )}
+                    </div>
+                    <input id="upload" type="file" accept='image/*' onChange={handleFileChange} className="hidden" />
+
                     <label htmlFor="projectName" className="block text-gray-700 font-bold">Pavadinimas</label>
                     <input type="text" id="projectName" placeholder="Pavadinimas" className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-500" value={project.name} onChange={(e) => setProject({ ...project, name: e.target.value })} />
                     {formErrors.name && <div className="text-red-500">{formErrors.name}</div>}
@@ -83,7 +148,7 @@ export default function EditProjectPage({ params }: { params: { id: number } }) 
                     <select
                         id="codebase_visibility"
                         className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600"
-                        value={project.codebase_visibility} onChange={(e) => setProject({ ...project, codebase_visibility: e.target.value})}
+                        value={project.codebase_visibility} onChange={(e) => setProject({ ...project, codebase_visibility: e.target.value })}
                     >
                         <option value="public">Vieša</option>
                         <option value="private">Privati</option>
