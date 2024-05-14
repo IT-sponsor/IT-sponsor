0729aef3889ce6097e52feb2b994d8e010801752
Aistis Draugelis <aistis.draugelis@ktu.edu>
2024-05-14 14:29:16 +0300

    +

--- app/components/User/UserCard.tsx
+++ app/components/User/UserCard.tsx
@@ -55,7 +55,7 @@ const UserCard: React.FC<UserCardProps> = ({
   const [issue, setIssue] = useState({} as Issue)
   const [image, setImage] = useState({} as string)
 
-  useEffect(() => {
+  async function fetchIssue() {
     fetch(`/api/project/${project_id}/issues/${issueId}`, {
       method: 'GET',
       headers: {
@@ -75,7 +75,8 @@ const UserCard: React.FC<UserCardProps> = ({
         console.log('Error fetching issue:', error.message)
         setIssue({} as Issue)
       })
-
+  }
+  async function fetchImage() {
     fetch(`/api/image/${image_id}`, {
       method: 'GET',
       headers: {
@@ -84,94 +85,94 @@ const UserCard: React.FC<UserCardProps> = ({
     })
       .then((res) => {
         if (!res.ok) {
-          console.error(`HTTP error! Status: ${res.status}`)
+          throw new Error(`HTTP error! Status: ${res.status}`)
         }
         return res.json()
       })
       .then((data) => {
-        if (data && data.image) {
-          const logoData = data.image.data
-          const base64String = Buffer.from(logoData).toString('base64')
-          setImage(`data:image/jpeg;base64,${base64String}`)
-        }
+        setImage(data.image)
       })
       .catch((error) => {
-        console.error('Error fetching image:', error.message)
+        console.log('Error fetching image:', error.message)
+        setImage('')
       })
+  }
+
+  useEffect(() => {
+    fetchIssue()
+    fetchImage()
   })
 
-  if (!issue) return null
+  if (!issue || Object.keys(issue).length === 0) return null
 
   if (
     !fullName.toLowerCase().includes(searchQuery.toLowerCase()) &&
     !issue.title?.toLowerCase().includes(searchQuery.toLowerCase())
   )
-    return null
-
-  return (
-    <div className="flex-grow px-4 py-2 mt-2 w-full rounded-xl border-2 border-gray-100 bg-white">
-      <div className="flex items-center justify-between">
-        <Link href={`/profile/${user.id}`}>
-          <div className="flex items-center space-x-4 shrink-0 hover:bg-green-100 rounded-lg pr-1">
-            <div className="w-16 h-16 bg-gray-200 rounded-full flex items-center justify-center">
-              {image ? (
-                <img
-                  alt={fullName}
-                  width={100}
-                  height={100}
-                  className="rounded-full"
-                  src={image}
-                />
-              ) : (
-                <span className="text-gray-500">Nėra nuotraukos</span>
-              )}
+    return (
+      <div className="flex-grow px-4 py-2 mt-2 w-full rounded-xl border-2 border-gray-100 bg-white">
+        <div className="flex items-center justify-between">
+          <Link href={`/profile/${user.id}`}>
+            <div className="flex items-center space-x-4 shrink-0 hover:bg-green-100 rounded-lg pr-1">
+              <div className="w-16 h-16 bg-gray-200 rounded-full flex items-center justify-center">
+                {image ? (
+                  <img
+                    alt={fullName}
+                    width={100}
+                    height={100}
+                    className="rounded-full"
+                    src={image}
+                  />
+                ) : (
+                  <span className="text-gray-500">Nėra nuotraukos</span>
+                )}
+              </div>
+              <div className="flex flex-col">
+                <a
+                  href={profileUrl}
+                  className="font-semibold text-lg hover:bg-green-100 rounded-lg px-1"
+                >
+                  {fullName}
+                </a>
+              </div>
             </div>
-            <div className="flex flex-col">
-              <a
-                href={profileUrl}
-                className="font-semibold text-lg hover:bg-green-100 rounded-lg px-1"
-              >
-                {fullName}
-              </a>
-            </div>
-          </div>
-        </Link>
-        <div className="flex items-center overflow-y-auto ">
-          <Link href={`/project/${project_id}/issue/${issueId}`}>
-            <span className="text-gray-700 hover:underline font-italic mr-4 ml-8 flex-col break-words flex-wrap">
-              {issue.title}
-            </span>
           </Link>
-          {type === 'applied' && issue.status === 'open' ? (
-            <button
-              onClick={() => onAssign(id, issueId)}
-              className="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded-md"
-            >
-              Priskirti
-            </button>
-          ) : (
-            issue.status === 'open' && (
-              <>
-                <button
-                  onClick={() => onCompleted(id, issueId)}
-                  className="bg-green-500 hover:bg-green-700 text-white px-4 py-2 mr-2 rounded-md"
-                >
-                  Atliko pataisymą
-                </button>
-                <button
-                  onClick={() => onRemove(id, issueId)}
-                  className="bg-red-500 hover:bg-red-700 text-white px-4 py-2 rounded-md"
-                >
-                  Pašalinti
-                </button>
-              </>
-            )
-          )}
-          {issue.status === 'closed' && <div>This shouldnt happen</div>}
+          <div className="flex items-center overflow-y-auto ">
+            <Link href={`/project/${project_id}/issue/${issueId}`}>
+              <span className="text-gray-700 hover:underline font-italic mr-4 ml-8 flex-col break-words flex-wrap">
+                {issue.title}
+              </span>
+            </Link>
+            {type === 'applied' && issue.status === 'open' ? (
+              <button
+                onClick={() => onAssign(id, issueId)}
+                className="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded-md"
+              >
+                Priskirti
+              </button>
+            ) : (
+              issue.status === 'open' && (
+                <>
+                  <button
+                    onClick={() => onCompleted(id, issueId)}
+                    className="bg-green-500 hover:bg-green-700 text-white px-4 py-2 mr-2 rounded-md"
+                  >
+                    Atliko pataisymą
+                  </button>
+                  <button
+                    onClick={() => onRemove(id, issueId)}
+                    className="bg-red-500 hover:bg-red-700 text-white px-4 py-2 rounded-md"
+                  >
+                    Pašalinti
+                  </button>
+                </>
+              )
+            )}
+            {issue.status === 'closed' && <div>This shouldnt happen</div>}
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
 }
 
 export default UserCard
